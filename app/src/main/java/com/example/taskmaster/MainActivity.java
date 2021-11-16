17c640d5c067d70bd7eb5704b3dccaa01bfdb41a
STUDENT <qutadaalmomani@yahoo.com>
2021-11-17 01:55:47 +0200

    lab 28

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,14 +13,33 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button allTaskButton = findViewById(R.id.allTask);
         Button addTaskButton = findViewById(R.id.addTask);
+
+        List<Task> allTask = new ArrayList<Task>();
+        allTask.add(new Task("Task1","TaskBody","complete"));
+        allTask.add(new Task("Task2","TaskBody","in progress"));
+        allTask.add(new Task("Task3","TaskBody","in progress"));
+        allTask.add(new Task("Task4","TaskBody","assigned"));
+        allTask.add(new Task("Task5","TaskBody","assigned"));
+        allTask.add(new Task("Task6","TaskBody","new"));
+        allTask.add(new Task("Task7","TaskBody","new"));
+
+
+        RecyclerView recyclerView = findViewById(R.id.dddd);
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        recyclerView.setAdapter(new TaskAdapter(allTask));
+
 
         allTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,41 +56,41 @@ addTaskButton.setOnClickListener(new View.OnClickListener() {
     }
 });
 
-        Button Button1 = (Button) findViewById(R.id.Button1);
-        Button Button2=(Button) findViewById(R.id.Button2);
-        Button Button3 =(Button) findViewById(R.id.Button3);
+//        Button Button1 = (Button) findViewById(R.id.Button1);
+//        Button Button2=(Button) findViewById(R.id.Button2);
+//        Button Button3 =(Button) findViewById(R.id.Button3);
         Button settingButton=(Button) findViewById(R.id.settingButton);
-        Button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String button1 = Button1.getText().toString();
-                Intent goToDetailPage = new Intent(MainActivity.this,TaskDetail.class);
-                goToDetailPage.putExtra("task detail",button1);
-                startActivity(goToDetailPage);
-
-            }
-
-        });
-        Button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String button2 = Button2.getText().toString();
-                Intent goToDetailPage = new Intent(MainActivity.this,TaskDetail.class);
-                goToDetailPage.putExtra("task detail",button2);
-                startActivity(goToDetailPage);
-
-            }
-        });
-        Button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String button3 = Button3.getText().toString();
-                Intent goToDetailPage = new Intent(MainActivity.this,TaskDetail.class);
-                goToDetailPage.putExtra("task detail",button3);
-                startActivity(goToDetailPage);
-
-            }
-        });
+//        Button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String button1 = Button1.getText().toString();
+//                Intent goToDetailPage = new Intent(MainActivity.this,TaskDetail.class);
+//                goToDetailPage.putExtra("task detail",button1);
+//                startActivity(goToDetailPage);
+//
+//            }
+//
+//        });
+//        Button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String button2 = Button2.getText().toString();
+//                Intent goToDetailPage = new Intent(MainActivity.this,TaskDetail.class);
+//                goToDetailPage.putExtra("task detail",button2);
+//                startActivity(goToDetailPage);
+//
+//            }
+//        });
+//        Button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String button3 = Button3.getText().toString();
+//                Intent goToDetailPage = new Intent(MainActivity.this,TaskDetail.class);
+//                goToDetailPage.putExtra("task detail",button3);
+//                startActivity(goToDetailPage);
+//
+//            }
+//        });
 
         settingButton.setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/com/example/taskmaster/TaskAdapter.java
@@ -0,0 +1,63 @@
+package com.example.taskmaster;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+    List<Task> allTask = new ArrayList<Task>();
+
+    public TaskAdapter(List<Task> allTask) {
+        this.allTask = allTask;
+    }
+    public static class TaskViewHolder extends RecyclerView.ViewHolder{
+
+        public Task task;
+        public View itemView;
+
+        public TaskViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.itemView = itemView;
+        }
+
+    }
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_blank,parent,false);
+        TaskViewHolder taskViewHolder = new TaskViewHolder(view);
+        return taskViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        holder.task = allTask.get(position);
+
+        TextView name = holder.itemView.findViewById(R.id.name);
+        TextView body = holder.itemView.findViewById(R.id.body);
+        TextView state = holder.itemView.findViewById(R.id.state);
+
+        name.setText(holder.task.name);
+        body.setText(holder.task.body);
+        state.setText(holder.task.state);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return allTask.size();
+    }
+
+
+
+
+}
